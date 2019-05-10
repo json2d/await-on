b0222989876f67b7382a612914bb7eef3b5f6c43
Julien Vanier <julien@particle.io>
2019-05-10 09:14:58 -0400

    Make on work with any Promises/A+ object with a then function

--- test/core.js
+++ test/core.js
@@ -1,15 +1,25 @@
 const tap = require('tap')
+const Bluebird = require('bluebird')
 const on = require('../lib/await-on')
 const ANSWER = 42
 const ERROR = new Error('Need more time to figure that out')
 
-let waitOne = (answer=ANSWER) => new Promise(resolve => setTimeout(() => resolve(answer),1000))
+let waitOne = (answer=ANSWER, PromiseConstructor = Promise) => new PromiseConstructor(resolve => setTimeout(() => resolve(answer),10))
 let waitOneAndThrow = () => waitOne().then(res => Promise.reject(ERROR))
 
 tap.test("on", async function (t) {
   t.plan(2)
 
 	const [err,res] = await on(waitOne())
+
+	t.notOk(err)
+	t.equal(res,ANSWER)
+})
+
+tap.test("on thenable", async function (t) {
+  t.plan(2)
+
+	const [err,res] = await on(waitOne(ANSWER, Bluebird))
 
 	t.notOk(err)
 	t.equal(res,ANSWER)
